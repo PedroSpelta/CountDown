42dfd5a912b3a580dd591b2876e58a2aed08b57e
Pedro Henrique Spelta <80366307+PedroSpelta@users.noreply.github.com>
2022-03-11 16:18:46 -0300

    feat: header component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import Header from './src/components/header/header';
 
 const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <ScrollView>
+
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header:React.FC = () => {
+  return <View>
+    <Text>eae</Text>
+  </View>;
+}
+
+export default Header;
